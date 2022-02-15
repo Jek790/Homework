ead2a226ec889267055d700f7f85c045e8b605ef
Evgeniy Tsurko <jek790@gmail.com>
2022-02-15 13:09:39 +0400

    writing a code in test file

--- src/test.java
+++ src/test.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class test {
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        System.out.println("String for your entered is " +string);
+    }
 }
