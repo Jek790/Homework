1e519fe677f1f33b63274568fdfcb8d3da877c2d
Evgeniy Tsurko <jek790@gmail.com>
2022-02-15 13:18:47 +0400

    delete test file

--- src/test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class test {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String string = scanner.nextLine();
-        System.out.println("String for your entered is " +string);
-    }
-}
