4cf3429890fdcbd6b7ba4933289f5a0dd6224942
Evgeniy Tsurko <jek790@gmail.com>
2022-02-16 14:13:58 +0400

    The initial value of the counter variable has been changed The for loop has been replaced with the forEach loop Excluded the use of the counter when printing an array

--- src/Homework06/Main2.java
+++ src/Homework06/Main2.java
@@ -1,5 +1,7 @@
 package Homework06;
 
+import java.util.Arrays;
+
 public class Main2 {
 
     /**
@@ -9,20 +11,19 @@ public class Main2 {
     public  static void autoSorting(int[] array) {
         int[] outputArray = new int[array.length];
         int indexOutputArray = 0;
-        for (int i = indexOutputArray; i < array.length; i++) {
-            if(array[i] != 0) {
-                outputArray[indexOutputArray] = array[i];
+        for (int i : array) {
+            if (i != 0) {
+                outputArray[indexOutputArray] = i;
                 indexOutputArray++;
             }
 
         }
-        for (int j : outputArray) {
-            System.out.print(j + ", ");
-        }
-        System.out.println();
+        System.out.println(Arrays.toString(outputArray));
     }
     public static void main(String[] args) {
         int[] massive = {12,0,23,4,32,543,6,0,4,54,24,0,1,95,40,0,23};
+        int[] massive2 = {235,34,21,5,343,728,0,23,0,123,4,9,45,325,0,5,32,657,0,64};
         autoSorting(massive);
+        autoSorting(massive2);
     }
 }
